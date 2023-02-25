90175f8e1f10a1312594110f55d5b5375b4cb7dd
Álvaro <ainiguezdislaa@gmail.com>
2023-02-25 11:52:39 +0100

    ejercicio terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,7 @@
-package aplicacion;public class Principal {
+package aplicacion;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("El valor de la constante de oro es: " + dominio.ej2_13_lambda.comparar(1.618));
+    }
 }
--- src/dominio/ej2_13_lambda.java
+++ src/dominio/ej2_13_lambda.java
@@ -1,2 +1,21 @@
-package dominio;public class ej2_13_lambda {
+package dominio;
+import java.util.stream.Stream;
+
+public class ej2_13_lambda {
+    public static int fib12() {
+        return Stream.iterate(new int[]{0, 1}, f -> new int[]{f[1], f[0] + f[1]}).limit(12).map(f -> f[0]).reduce(0, (a, b) -> b);
+    }
+
+    public static int fib13() {
+        return Stream.iterate(new int[]{0, 1}, f -> new int[]{f[1], f[0] + f[1]}).limit(13).map(f -> f[0]).reduce(0, (a, b) -> b);
+    }
+
+    public static boolean comparar(double a) {
+        double fib = fib13() / fib12();
+        if (a == fib) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
